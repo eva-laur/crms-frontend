f894f2c05a92ccbe55b777a1e2058dfe523892df
eva-laur <evalaure014@gmail.com>
2026-07-10 08:34:27 +0100

    fix deployment

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 30000,
+        });
         console.log('MongoDB connected');
         } catch (error) {
             console.log(error);
